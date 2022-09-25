79180a9a51db400ffd3b7603e84307eb7386bdb3
UMAR KHAN <umargd123@gmail.com>
2022-09-26 01:08:12 +0530

    Create Java increments.java

---
+++ Java increments.java
@@ -0,0 +1,26 @@
+//comment the logic behind this code
+public class Program
+{
+    public static void main(String[] args)         
+    {
+        int b=1,a=1;
+        System.out.println(a);
+        a+=b;
+        System.out.println(a);
+        a+=+b;
+        System.out.println(a);
+        a+=++b;
+        System.out.println(a);
+        a=a+b;
+        System.out.println(a);
+        a-=-b;
+        System.out.println(a);
+        a-=+b;
+        System.out.println(a);
+        a-=++b;
+        System.out.println(a);
+        a+=--b;
+        System.out.println(a);  
+        
+    }
+}
