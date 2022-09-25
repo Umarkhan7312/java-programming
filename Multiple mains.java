11070ab517ad30adf6ce8bae1ebe2cf996795a2e
UMAR KHAN <umargd123@gmail.com>
2022-09-26 01:09:18 +0530

    Create Multiple mains.java

---
+++ Multiple mains.java
@@ -0,0 +1,17 @@
+// Guess the output 
+public class Program
+{
+ static int i= main();
+ 
+ public static void main(String[] args) 
+ {
+        System.out.println(" main "+i);
+        
+ }
+    
+ public static int main()
+ {
+        System.out.print(" hi ");
+  return 10;
+ }
+}
